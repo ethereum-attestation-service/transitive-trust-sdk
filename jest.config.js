f9bedc27e183f3cda95f54b890d91a15fe670775
Steve Dakh <slavik0329@gmail.com>
2025-07-30 16:11:16 -0400

    Add coverage config and script to Jest setup

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,20 @@ module.exports = {
     "^.+\\.ts$": "ts-jest",
   },
   testMatch: ["**/tests/**/*.test.ts"],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/index.ts"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
